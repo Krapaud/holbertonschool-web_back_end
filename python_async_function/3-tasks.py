540baaf21d63e353f899e0c425658cc3c8e791dc
Krapaud <mur.mickael@gmail.com>
2025-11-26 11:12:35 +0100

    Add task_wait_random function to create asyncio Task from wait_random

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 """Module for creating asyncio Tasks from coroutines."""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Create and return an asyncio Task for wait_random(max_delay)."""
+    return asyncio.create_task(wait_random(max_delay))
